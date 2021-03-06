4d5f1dd34adc9462ecfa8325ba1fdafee874f974
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-05 19:24:24 -0500

    commit tree bcea69e7d1321575f1c787857f7350929bbc60b6

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,36 @@ class App extends React.Component {
   constructor() {
     super()
 
+    this.rootPath = '/api/pets'
+
     this.state = {
       pets: [],
       filters: {
         type: 'all'
       }
     }
+  }
+
+  handleFilter = (e) => {
+    this.setState(
+      Object.assign({}, this.state, {
+        filters: {
+          type: e.target.value
+        }
+      })
+    )
+  }
+
+  findPets = () => {
+    const submitURL = this.state.filters.type === 'all' ? this.rootPath : this.rootPath + `?type=${this.state.filters.type}`
+    fetch(submitURL).then(resp => resp.json()).then(json => this.setState({pets: json}))
+  }
+
+  onAdoptPet = (id) => {
+    this.setState(prevState => {
+      const index = prevState.pets.findIndex(pet => pet.id === id)
+      return {pets: [...prevState.pets.slice(0, index), Object.assign({}, prevState.pets[index], {isAdopted: true}), ...prevState.pets.slice(index + 1)]}
+    })
   }
 
   render() {
@@ -24,10 +48,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleFilter} onFindPetsClick={this.findPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
